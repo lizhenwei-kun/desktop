070f239b136c07383cdd361f03bbec4b0edd58e4
lizhenwei-kun <lzwlfw@163.com>
2026-07-30 14:59:55 +0800

    优化构建脚本：build.bat 添加 -s -w -trimpath 减少包体积；新增 build_debug.bat 支持附加调试

--- build_debug.bat
+++ build_debug.bat
@@ -2,6 +2,5 @@ set PATH=D:\mingw64\bin;%PATH%
 set CGO_ENABLED=1
 cd /d e:\work\desktop_go
 D:\work\golang_git\gopath\bin\rsrc -manifest desktop_go.exe.manifest -ico internal/resources/app.ico -o rsrc.syso
-go build -tags walk_use_cgo -ldflags="-H windowsgui -s -w" -trimpath -v -o desktop_go.exe ./main.go
-:: upx --lzma --best desktop_go.exe
-:: desktop_go.exe -d
+go build -tags walk_use_cgo -gcflags="all=-N -l" -ldflags="-H windowsgui" -v -o desktop_go_debug.exe ./main.go
+:: desktop_go_debug.exe -d
